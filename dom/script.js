eafe7c0989143115493d75f32ab6f499e7e474e5
bbbbbbbb123 <122397079+bbbbbbbb123@users.noreply.github.com>
2026-02-02 00:16:36 +0530

    dom learning start with js

---
+++ dom/script.js
@@ -0,0 +1,17 @@
+  var card = document.querySelector('.card') ;
+
+  var btn = document.querySelector('button');
+
+  btn.addEventListener('click', function(){
+    var c1 = Math.floor(Math.random()*256) ;
+    var c2 = Math.floor(Math.random()*256)
+    var c3 = Math.floor(Math.random()*256)
+
+    card.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+    console.log(c1,c2,c3);
+    
+})
+
+
+   
+//    btn.style.backgroundColor ="blue";
